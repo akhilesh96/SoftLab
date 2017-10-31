778f5a857c7b73f5383364124918351627e9c289
akhilesh96 <akhileshgundala@gmail.com>
2017-10-31 19:37:22 +0530

    second

--- app/src/main/java/com/example/android/softlab/AddStudentActivity.java
+++ app/src/main/java/com/example/android/softlab/AddStudentActivity.java
@@ -26,7 +26,7 @@ import java.util.List;
 
 public class AddStudentActivity extends AppCompatActivity {
 
-    EditText regNo,course,name,fname,mname,address,district,city,state,pno,sex,email;
+    EditText regNo,course,name,fname,mname,address,district,city,state,pno,sex,dob,email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,19 +35,37 @@ public class AddStudentActivity extends AppCompatActivity {
         regNo = (EditText)findViewById(R.id.regNo);
         course=(EditText)findViewById(R.id.course);
         name=(EditText)findViewById(R.id.name);
-        fname=(EditText)findViewById(R.id.course);
-        mname=(EditText)findViewById(R.id.course);
-        course=(EditText)findViewById(R.id.course);
-        course=(EditText)findViewById(R.id.course);
-        course=(EditText)findViewById(R.id.course);
-        course=(EditText)findViewById(R.id.course);
+        fname=(EditText)findViewById(R.id.fathersName);
+        mname=(EditText)findViewById(R.id.mothersName);
+        address=(EditText)findViewById(R.id.addrss);
+        district=(EditText)findViewById(R.id.district);
+        city=(EditText)findViewById(R.id.city);
+        state=(EditText)findViewById(R.id.state);
+        pno=(EditText)findViewById(R.id.phoneNumber);
+        sex=(EditText)findViewById(R.id.sex);
+        dob=(EditText)findViewById(R.id.dob);
+        email=(EditText)findViewById(R.id.email);
+
 
     }
 
     public void addStudent(View view){
         Toast.makeText(this, "add student", Toast.LENGTH_SHORT).show();
+        String s1=regNo.getText().toString();
+        String s2=course.getText().toString();
+        String s3=name.getText().toString();
+        String s4=fname.getText().toString();
+        String s5=mname.getText().toString();
+        String s6=address.getText().toString();
+        String s7=district.getText().toString();
+        String s8=city.getText().toString();
+        String s9=state.getText().toString();
+        String s10=pno.getText().toString();
+        String s11=sex.getText().toString();
+        String s12=dob.getText().toString();
+        String s13=email.getText().toString();
 
-//        new MainActivity.ExecuteTask().execute(s1,s2);
+       new ExecuteTask().execute(s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13);
     }
     class ExecuteTask extends AsyncTask<String, Integer, String>
     {
@@ -62,9 +80,8 @@ public class AddStudentActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String result) {
-//            progressBar.setVisibility(View.GONE);
-//            //progess_msz.setVisibility(View.GONE);
-//            Toast.makeText(MainActivity.this, result,Toast.LENGTH_SHORT).show();
+            //progess_msz.setVisibility(View.GONE);
+            Toast.makeText(AddStudentActivity.this, result,Toast.LENGTH_SHORT).show();
 
         }
 
@@ -75,11 +92,23 @@ public class AddStudentActivity extends AppCompatActivity {
         try
         {
             HttpClient httpClient=new DefaultHttpClient();
-            HttpPost httpPost=new HttpPost("http://slab-env.us-west-2.elasticbeanstalk.com/Login");
+            HttpPost httpPost=new HttpPost("http://slab-env.us-west-2.elasticbeanstalk.com/AddStudent");
 
             List<NameValuePair> list=new ArrayList<NameValuePair>();
-            list.add(new BasicNameValuePair("name", valuse[0]));
-            list.add(new BasicNameValuePair("pass",valuse[1]));
+            list.add(new BasicNameValuePair("regNo", valuse[0]));
+            list.add(new BasicNameValuePair("course",valuse[1]));
+            list.add(new BasicNameValuePair("name",valuse[2]));
+            list.add(new BasicNameValuePair("fname",valuse[3]));
+            list.add(new BasicNameValuePair("mname",valuse[4]));
+            list.add(new BasicNameValuePair("address",valuse[5]));
+            list.add(new BasicNameValuePair("district",valuse[6]));
+            list.add(new BasicNameValuePair("city",valuse[7]));
+            list.add(new BasicNameValuePair("state",valuse[8]));
+            list.add(new BasicNameValuePair("pno",valuse[9]));
+            list.add(new BasicNameValuePair("sex",valuse[10]));
+            list.add(new BasicNameValuePair("dob",valuse[11]));
+            list.add(new BasicNameValuePair("email",valuse[12]));
+
             httpPost.setEntity(new UrlEncodedFormEntity(list));
             HttpResponse httpResponse=  httpClient.execute(httpPost);
 
@@ -104,7 +133,7 @@ public class AddStudentActivity extends AppCompatActivity {
                 finish();
             }
             else{
-//               showToast("Login Error. Please check your credentials and try again.");
+               showToast("Login Error. Please check your credentials and try again.");
             }
         }
         catch(Exception exception)  {
@@ -118,5 +147,8 @@ public class AddStudentActivity extends AppCompatActivity {
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
         finish();
+    }
+    public void showToast(String msg){
+        Toast.makeText(AddStudentActivity.this, msg, Toast.LENGTH_SHORT ).show();
     }
 }
